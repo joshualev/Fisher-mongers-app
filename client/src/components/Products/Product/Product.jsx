b34d99122a4fb1e38b8a8c3b5c5fffe5ea6ff149
jlevinson <joshua.lev1995@gmail.com>
2022-08-01 17:25:31 +1000

    added show page

--- client/src/components/Products/Product/Product.jsx
+++ client/src/components/Products/Product/Product.jsx
@@ -5,21 +5,21 @@ import CardActions from '@mui/material/CardActions';
 import CardContent from '@mui/material/CardContent';
 import CardMedia from '@mui/material/CardMedia';
 import Typography from '@mui/material/Typography';
-import IconButton from '@mui/material/IconButton';
-import ShoppingCartIcon from '@mui/icons-material/ShoppingCart';
-import { Divider } from '@mui/material';
+import Button from '@mui/material/Button';
 
 
-
-// <Typography variant="p" color="textSecondary">
-// Caught by {fish.caughtBy}
-// </Typography>
 const Product = ( {fish} ) => {
-    console.log(fish)
   return (
     <>
     <Card key={fish._id} sx={{ maxWidth: '100%' }}>
+        {/* <div style={{ position: "relative" }}> */}
         <CardMedia sx={{height: 0, paddingTop: '56.25%', }} image={fish.imageURL} title={fish.species}/>
+            {/* <div style={{position:"absolute", color:"white", top:10, left: "20%", transform:"translateX(-50%)",}}>        
+                <Typography variant="body2" sx={{color: palette.common.white}} >
+                    Caught by {fish.caughtBy}
+                </Typography>
+            </div>
+        </div> */}
         <CardContent sx={{justifyContent: 'space-between', display:'flex'}}>
             <Box sx ={{ height: '100%', display: 'flex', flexDirection: 'column' }}>
             <Typography variant="h5" gutterBottom>
@@ -34,10 +34,17 @@ const Product = ( {fish} ) => {
             </Typography>
         </CardContent>
         <CardActions disableSpacing sx={{display: 'flex',
-        justifyContent: 'flex-end'}}>
-            <IconButton aria-label="Add to Cart">
-            < ShoppingCartIcon />
-            </IconButton>
+        justifyContent: 'center'}}>
+            <Card sx={{mb:1}}>
+                <Button  sx={{pt:1, pb:1}} variant="contained" size="medium" color="primary">-</Button>
+                <Typography variant="p" sx={{p:2}}>
+                    6
+                </Typography>
+                <Button sx={{pt:1, pb:1}} variant="contained"  size="medium" color="primary">+</Button>
+                <div>
+                <Button sx={{mt:1, width:'100%'}} variant="contained" color="warning">Add to cart</Button>
+                </div>
+            </Card>
         </CardActions>
     </Card>
     </>
---
+++ client/src/components/Show/Show.jsx
@@ -0,0 +1,87 @@
+
+import * as React from 'react';
+import Box from '@mui/material/Box';
+import Button from '@mui/material/Button';
+import Card from '@mui/material/Card';
+import CardActions from '@mui/material/CardActions';
+import CardContent from '@mui/material/CardContent';
+import CardHeader from '@mui/material/CardHeader';
+import CardMedia from '@mui/material/CardMedia';
+import Typography from '@mui/material/Typography';
+import Container from '@mui/material/Container';
+
+const Show = () => {
+    return(
+        <>
+{/* Hero unit */}
+<Container disableGutters maxWidth="sm" component="main" sx={{ pt: 8, pb: 6 }}>
+        <Typography
+          component="h1"
+          variant="h2"
+          align="center"
+          color="text.primary"
+          gutterBottom
+        >
+          FISH NAME
+        </Typography>
+        <Typography variant="h5" align="center" color="text.secondary" component="p">
+        DESCRIPTION HERE DESCRIPTION HERE DESCRIPTION HERE DESCRIPTION HERE DESCRIPTION HERE DESCRIPTION HERE DESCRIPTION HERE DESCRIPTION HERE DESCRIPTION HERE DESCRIPTION HERE DESCRIPTION HERE DESCRIPTION HERE DESCRIPTION HERE
+        </Typography>
+      </Container>
+      {/* End hero unit */}
+      <Container maxWidth="md" component="main" sx={{width:'50%', align: 'center', justifyContent:'center'}}>
+              <Card >
+                <CardHeader
+                  title="TITLE"
+                  subheader="SUBHEAD"
+                  titleTypographyProps={{ align: 'center' }}
+                  subheaderTypographyProps={{
+                    align: 'center',
+                  }}
+                  sx={{
+                    backgroundColor: (theme) =>
+                      theme.palette.mode === 'light'
+                        ? theme.palette.grey[200]
+                        : theme.palette.grey[700],
+                  }}
+                />
+                <CardContent>
+                <CardMedia 
+                    sx={{height: 0, paddingTop: '56.25%', }} 
+                    image="https://www.fishfiles.com.au/-/media/fishfiles/images/preparing-seafood/110830_erd_snapper__0374sm.ashx?h=453&w=1000&la=en&hash=1D1B5A009344F3AD329992DAD21F959C16B17529" 
+                    title="Fish Species"
+                />
+                  <Box
+                    sx={{
+                      display: 'flex',
+                      justifyContent: 'center',
+                      alignItems: 'baseline',
+                      mb: 2,
+                    }}
+                  >
+                  </Box>
+                </CardContent>
+                <CardActions disableSpacing sx={{display: 'flex',
+                justifyContent: 'center'}}>
+                    <Card sx={{mb:1}}>
+                        <Button  sx={{pt:1, pb:1}} variant="contained" size="medium" color="primary">-</Button>
+                        <Typography variant="p" sx={{p:2}}>
+                            6
+                        </Typography>
+                        <Button sx={{pt:1, pb:1}} variant="contained"  size="medium" color="primary">+</Button>
+                        <div>
+                            <Button sx={{mt:1, width:'100%'}} variant="contained" color="warning">Add to cart</Button>
+                        </div>
+                    </Card>
+                </CardActions>
+              </Card>
+            
+   
+      </Container>
+
+
+        </>
+    )
+}
+
+export default Show
