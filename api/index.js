628983b6d32965415f3deb786b47114f514398f9
jlevinson <joshua.lev1995@gmail.com>
2022-08-01 15:14:21 +1000

    integrated with db

--- api/index.js
+++ api/index.js
@@ -1,30 +1,33 @@
 require('dotenv').config()
 const express = require('express')
 const mongoose = require('mongoose')
-// const cors = require('cors')
+const cors = require('cors')
 const app = express()
 const PORT = process.env.PORT
 const dbURL = process.env.MONGODB_URL
-// const whitelist = [
-//     'http://localhost:4001'
-// ]
+const whitelist = [
+    'http://localhost:4001'
+]
 
+const fishController = require('./controllers/Fish')
 
 app.use(express.json())
 app.use(express.urlencoded({ extended: true }));
-// app.use(cors({
-//     origin: (origin, cb) => {
-//         if (whitelist.indexOf(origin) !== -1) {
-//             cb(null, true)
-//         } else {
-//             cb(new Error())
-//         }
-//     }
-// }))
+app.use(cors({
+    origin: (origin, cb) => {
+        if (whitelist.indexOf(origin) !== -1) {
+            cb(null, true)
+        } else {
+            cb(new Error())
+        }
+    }
+}))
 
+
+app.use('/fish', fishController)
 
 mongoose.connect(dbURL, () => {
-    console.log('connected to doggos db')
+    console.log('connected to fish db')
 })
 app.listen(PORT, () => {
     console.log('listening on port: ', PORT)
