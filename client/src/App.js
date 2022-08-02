29b83084092599f73eff7a671126c15269cc1cb2
Evan Comtesse <evancomtesse@Evans-MacBook-Pro.local>
2022-08-02 17:42:32 +1000

    edit view added (pre merge)

--- client/src/App.js
+++ client/src/App.js
@@ -1,5 +1,5 @@
 import { Routes, Route, useNavigate } from 'react-router-dom'
-import { useState, useEffect} from 'react'
+import { useState, useEffect } from 'react'
 import Products from './components/Products/Products'
 import Checkout from './components/Checkout/Checkout'
 import Navbar from './components/Navbar/Navbar'
@@ -8,58 +8,69 @@ import Create from './components/Create/Create'
 import Footer from './components/Footer/Footer'
 import SignIn from './components/User/SignIn'
 import SignUp from './components/User/SignUp'
+import Edit from './components/Edit/Edit'
+
 const App = () => {
   const [fishList, setFishList] = useState(null)
 
-  const getFish = async() => {
+  const getFish = async () => {
     const url = 'http://localhost:4000/fish'
     const res = await fetch(url)
     const data = await res.json()
     setFishList(data)
   }
 
+  const navigate = useNavigate()
+
 
   useEffect(() => {
     getFish()
-  },[])
+  }, [])
+
+
+
   return (
     <div>
-      <Navbar/>
+      <Navbar />
 
       <Routes>
-        <Route 
-          path='/' 
+        <Route
+          path='/'
           element={fishList && <Products
-            fishList={fishList} 
+            fishList={fishList}
           />}
         />
-        <Route 
-          path='/:fishID' 
+        <Route
+          path='/:fishID'
           element={fishList && <Show
             fishList={fishList}
           />}
         />
-        <Route 
+        <Route
           path="/new"
-          element= {<Create />}
+          element={<Create handleNewFish={handleNewFish} />}
         />
         <Route
-          path='/checkout' 
+          path='/checkout'
           element={fishList && <Checkout
             fishList={fishList}
           />}
         />
-         <Route 
-          path='/signin' 
-          element={ <SignIn/>}
+        <Route
+          path='/signin'
+          element={<SignIn />}
+        />
+        <Route
+          path='/signup'
+          element={<SignUp />}
         />
         <Route 
-          path='/signup' 
-          element={ <SignUp/>}
+          path='/edit/:fishID'
+          element={fishList && <Edit fishList={fishList} handleEdit={handleEdit} />}
         />
       </Routes>
-      <Footer/>
-        
+      <Footer />
+
     </div>
   )
 }
