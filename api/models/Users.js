0c25998333859eefa31464409d602eb6c1a0ec7e
Dan S <drinkwithdan@gmail.com>
2022-08-02 16:37:26 +1000

    New user form wired up

--- api/models/Users.js
+++ api/models/Users.js
@@ -4,7 +4,7 @@ const Schema = mongoose.Schema
 const userSchema = Schema({
   username: { type: String, unique: true, required: true },
   password: { type: String, required: true },
-  isAdmin: { type: Boolean, default: false }
+  isAdmin: { type: Boolean, default: true }
 })
 
 const Users = mongoose.model("Users", userSchema)
--- client/src/components/User/SignUp.jsx
+++ client/src/components/User/SignUp.jsx
@@ -1,3 +1,4 @@
+import { useState } from "react"
 import { NavLink } from 'react-router-dom'
 import * as React from 'react';
 import Avatar from '@mui/material/Avatar';
@@ -13,79 +14,88 @@ import LockOutlinedIcon from '@mui/icons-material/LockOutlined';
 import Typography from '@mui/material/Typography';
 import Container from '@mui/material/Container';
 
-  export default function SignUp() {
-    const handleSubmit = (event) => {
-      event.preventDefault();
-      const data = new FormData(event.currentTarget);
-      console.log({
-        username: data.get('username'),
-        password: data.get('password'),
-      });
-    };
-  
-    return (
-        <Container component="main" maxWidth="xs">
-          <CssBaseline />
-          <Box
-            sx={{
-              marginTop: 8,
-              display: 'flex',
-              flexDirection: 'column',
-              alignItems: 'center',
-            }}
+const SignUp = () => {
+  const handleSubmit = async (event) => {
+    event.preventDefault()
+    const data = new FormData(event.currentTarget)
+    const newUser = {
+      username: data.get("username"),
+      password: data.get("password")
+    }
+    const res = await fetch("/users/register", {
+      method: "POST",
+      headers: { "Content-Type": "application/json" },
+      body: JSON.stringify(newUser)
+    })
+    const newData = await res.json()
+    console.log(newData);
+  };
+
+  return (
+    <Container component="main" maxWidth="xs">
+      <CssBaseline />
+      <Box
+        sx={{
+          marginTop: 8,
+          display: 'flex',
+          flexDirection: 'column',
+          alignItems: 'center',
+        }}
+      >
+        <Avatar sx={{ m: 1, bgcolor: 'secondary.main' }}>
+          <LockOutlinedIcon />
+        </Avatar>
+        <Typography component="h1" variant="h5">
+          Sign up
+        </Typography>
+        <Box component="form" noValidate onSubmit={handleSubmit} sx={{ mt: 3 }}>
+          <Grid container spacing={2}>
+            <Grid item xs={12}>
+              <TextField
+                required
+                fullWidth
+                name="username"
+                label="Username"
+                type="username"
+                id="username"
+                autoComplete="username"
+              />
+            </Grid>
+            <Grid item xs={12}>
+              <TextField
+                required
+                fullWidth
+                name="password"
+                label="Password"
+                type="password"
+                id="password"
+                autoComplete="new-password"
+              />
+            </Grid>
+          </Grid>
+
+          <Button
+            type="submit"
+            fullWidth
+            variant="contained"
+            sx={{ mt: 3, mb: 2 }}
           >
-            <Avatar sx={{ m: 1, bgcolor: 'secondary.main' }}>
-              <LockOutlinedIcon />
-            </Avatar>
-            <Typography component="h1" variant="h5">
-              Sign up
-            </Typography>
-            <Box component="form" noValidate onSubmit={handleSubmit} sx={{ mt: 3 }}>
-              <Grid container spacing={2}>
-              <Grid item xs={12}>
-                  <TextField
-                    required
-                    fullWidth
-                    name="username"
-                    label="Username"
-                    type="username"
-                    id="username"
-                    autoComplete="username"
-                  />
-                </Grid>
-                <Grid item xs={12}>
-                  <TextField
-                    required
-                    fullWidth
-                    name="password"
-                    label="Password"
-                    type="password"
-                    id="password"
-                    autoComplete="new-password"
-                  />
-                </Grid>
-              </Grid>
-              <NavLink to='/signin'>
-                <Button
-                  type="submit"
-                  fullWidth
-                  variant="contained"
-                  sx={{ mt: 3, mb: 2 }}
-                >
-                  Sign Up
-                </Button>
-              </NavLink>
-              <Grid container justifyContent="flex-end">
-                <Grid item>
-                  <NavLink to="/signin">
-                    <Link variant="body2">
-                      Already have an account? Sign in
-                    </Link>
-                  </NavLink>
-                </Grid>
-              </Grid>
-            </Box>
-          </Box>
-        </Container>
-    );
-  }
+            Sign Up
+          </Button>
+
+          <Grid container justifyContent="flex-end">
+            <Grid item>
+
+              <Link variant="body2">
+                Already have an account? Sign in
+              </Link>
+
+            </Grid>
+          </Grid>
+        </Box>
+      </Box>
+    </Container>
+  );
+}
+
+export default SignUp
