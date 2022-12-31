5f0a2258ebe30d58269cf5cf2f2a94a6266c94b3
Joshua Levinson <josh@192-168-1-106.tpgi.com.au>
2023-01-01 10:52:01 +1100

    reverting prev changes

--- server/index.js
+++ server/index.js
@@ -12,27 +12,27 @@ const app = express();
 const PORT = process.env.PORT || 3001;
 
 // ----------------------------------------------------------------
-const Fish = require("./models/Fish");
-const dummyFish = require("./seed/data.json");
+// const Fish = require("./models/Fish");
+// const dummyFish = require("./seed/data.json");
 // ----------------------------------------------------------------
 
 const dbURL = process.env.MONGODB_URL;
 
 // ----------------------------------------------------------------
-mongoose.connect(dbURL, async () => {
-  console.log("Connected to Fish db");
+// mongoose.connect(dbURL, async () => {
+//   console.log("Connected to Fish db");
 
-  console.log("Resetting Fish collection");
-  await Fish.collection.drop();
-  console.log("Fish collection dropped");
+//   console.log("Resetting Fish collection");
+//   await Fish.collection.drop();
+//   console.log("Fish collection dropped");
 
-  console.log("Inserting seed data");
-  const insertedFish = await Fish.insertMany(dummyFish);
-  console.log("Dummy Fish inserted");
-  console.log(insertedFish);
+//   console.log("Inserting seed data");
+//   const insertedFish = await Fish.insertMany(dummyFish);
+//   console.log("Dummy Fish inserted");
+//   console.log(insertedFish);
 
-  mongoose.connection.close();
-});
+//   mongoose.connection.close();
+// });
 // ----------------------------------------------------------------
 
 const MongoDBStore = mongoDBSession(session);
